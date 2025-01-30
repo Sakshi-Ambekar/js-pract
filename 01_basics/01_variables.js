300d2157901256cc1cce3ea46d320acb0fb46170
Sakshi-Ambekar <sakshiambekar0212@gmail.com>
2025-01-30 17:23:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 987654
+let accountEmail="sakshi@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+/* prefer not to use var 
+because of issue in block scope and functional scope */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
